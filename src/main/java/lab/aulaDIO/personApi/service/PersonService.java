c2b6e87fc63137a2b78b45c7d5c31c450b7e2517
Silas Santos Leite <estoudente.program@gmail.com>
2021-04-18 11:43:00 -0300

    Improved person creation functionality

--- src/main/java/lab/aulaDIO/personApi/service/PersonService.java
+++ src/main/java/lab/aulaDIO/personApi/service/PersonService.java
@@ -1,24 +1,29 @@
 package lab.aulaDIO.personApi.service;
 
+import lab.aulaDIO.personApi.dto.request.PersonDTO;
 import lab.aulaDIO.personApi.dto.response.MessageResponseDTO;
 import lab.aulaDIO.personApi.entity.Person;
+import lab.aulaDIO.personApi.mapper.PersonMapper;
 import lab.aulaDIO.personApi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created Person with ID " + savedPerson.getId())
