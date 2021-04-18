fac0a4cde134ba28c14d984958809c31b116961d
Silas Santos Leite <estoudente.program@gmail.com>
2021-04-18 11:56:26 -0300

    Developed list all operation

--- src/main/java/lab/aulaDIO/personApi/controller/PersonController.java
+++ src/main/java/lab/aulaDIO/personApi/controller/PersonController.java
@@ -5,9 +5,18 @@ import lab.aulaDIO.personApi.dto.response.MessageResponseDTO;
 import lab.aulaDIO.personApi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -25,5 +34,10 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
 
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 }
